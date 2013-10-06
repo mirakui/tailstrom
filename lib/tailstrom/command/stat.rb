50600ee1794a5411bebf64c4b8a3b21e81d9b298
Issei Naruta <naruta@cookpad.com>
2013-10-06 10:06:26 +0900

    Implement filter

--- lib/tailstrom/command/stat.rb
+++ lib/tailstrom/command/stat.rb
@@ -1,6 +1,6 @@
-require 'optparse'
 require 'tailstrom/counter'
 require 'tailstrom/table'
+require 'tailstrom/tail_reader'
 
 module Tailstrom
   module Command
@@ -12,20 +12,20 @@ module Tailstrom
         { :name => 'avg', :width => 15 }
       ]
 
-      def initialize(argv)
+      def initialize(options)
         @infile = $stdin
         @counter = Counter.new
         @table = Table.new SCHEMA
-        parse_option argv
+        @options = options
       end
 
       def run
-        Thread.start {
-          @infile.each_line do |line|
-            line = line.chomp
-            parse_line line
+        Thread.start do
+          reader = TailReader.new @infile, @options
+          reader.each_line do |line|
+            @counter << line[:value]
           end
-        }
+        end
 
         @table.print_header
 
@@ -43,25 +43,6 @@ module Tailstrom
         end
       rescue Interrupt
         exit 0
-      end
-
-      def parse_line(line)
-        columns = line.split @options[:delimiter]
-        value = @options[:field] ? columns[@options[:field]] : line
-        value = value =~ /\./ ? value.to_f : value.to_i
-        @counter << value
-      end
-
-      def parse_option(argv)
-        @options = {
-          :delimiter => "\t",
-          :interval => 1
-        }
-        opt = OptionParser.new(argv)
-        opt.on('-f [field]', Integer) {|v| @options[:field] = v - 1 }
-        opt.on('-d [delimiter]', String) {|v| @options[:delimiter] = v }
-        opt.on('-i [interval]', Integer) {|v| @options[:interval] = v }
-        opt.parse!
       end
     end
   end
--- lib/tailstrom/tail_reader.rb
+++ lib/tailstrom/tail_reader.rb
@@ -1,10 +1,28 @@
 module Tailstrom
   class TailReader
-    def initialize(infile)
-      @inflie = infile
+    def initialize(infile, options={})
+      @infile = infile
+      @options = options
     end
 
-    def run
+    def each_line
+      @infile.each_line do |line|
+        result = parse_line(line)
+        yield result if result
+      end
+    end
+
+    def parse_line(line)
+      columns = line.split @options[:delimiter]
+      value = @options[:field] ? columns[@options[:field]] : line
+      value = value =~ /\./ ? value.to_f : value.to_i
+
+      if @options[:filter]
+        v, cols = value, columns # shorthands
+        return nil unless binding.eval(@options[:filter])
+      end
+
+      { :line => line, :columns => columns, :value => value }
     end
   end
 end
